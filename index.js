542987df2961548df20f84681226518ed765fae6
Mr0indeterminant <92031523+Mr0indeterminant@users.noreply.github.com>
2023-10-03 23:01:44 +0530

    Initial commit

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("App connected to database");
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening to port: ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
   })
   .catch((error) => {
