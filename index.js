fc726d9965e1bd67d367af12abb2bd9ac73d6ef4
Mr0indeterminant <92031523+Mr0indeterminant@users.noreply.github.com>
2023-10-03 22:47:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import booksRoutes from "./routes/booksRoutes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to Bookstore App");
+});
+
+app.use("/books", booksRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
